d6195950d1598a3b9319b8dbbddf422bcb2f70d8
ssue0625 <ssue0625@gmail.com>
2019-01-27 17:50:21 +0900

    study

--- Tetris/TetrisModel.js
+++ Tetris/TetrisModel.js
@@ -1,11 +1,6 @@
 class TetrisModel {
     getData(funk) {
-        setInterval(()=>{
-            const price = 200 * Math.random();
-            //funk(price);
-            console.log(price);
-        }, 1000);
-        
+        const price = 200 * Math.random();
+        //funk(price);
     }
 }
-  
--- Tetris/TetrisViewModel.js
+++ Tetris/TetrisViewModel.js
@@ -1,22 +1,17 @@
 class TetrisViewModel {
-  constructor(display) {
-    this.tetrisModel = new TetrisModel();
-    this.tetrisView = new TetrisView(display);
-  }
-  showView(data) {
-    this.tetrisView.show(data);
-  }
-  run() {
-      //const data = this.tetrisModel.getData();
-      //this.tetrisView.show(data);
-      console.log('시작');
-      let sum = 0;
-      
-      this.tetrisModel.getData(this.showView.bind(this));
-      for (let i = 0; i < 990000000; i++) {
-          sum += i;
-      }
-      console.log('끝');
-  } 
+    constructor(display) {
+
+
+
+        this.tetrisModel = new TetrisModel();
+        this.tetrisView = new TetrisView(display);
+    }
+    showView(data) {
+        this.tetrisView.show(data);
+    }
+    run() {
+        //const data = this.tetrisModel.getData();
+        //this.tetrisView.show(data);
+        this.tetrisModel.getData(this.showView.bind(this));
+    }
 }
-  
