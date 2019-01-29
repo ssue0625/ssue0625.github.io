267fd5645d6fc2b40e08f3e8e41469bb81a89eea
rtos9er <rtos9er@chol.com>
2019-01-29 14:31:07 +0900

    모듈은 mjs 로... export, import && export default

---
+++ ModuleStudy/module1.mjs
@@ -0,0 +1,9 @@
+export function showModule1() {
+    console.log('module1 의 함수 이다');
+}
+export default class Module1Class {
+    show() {
+        console.log('Claass Module1의 show 함수 이다');
+    }   
+}
+//export {Module1Class, showModule1};
---
+++ ModuleStudy/module2.mjs
@@ -0,0 +1,4 @@
+const Abc = () => {
+    console.log('Module2의 abc 함수다');
+}
+export default Abc;
