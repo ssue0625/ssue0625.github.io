5e9545840c6480c7c95df9fc12ebed9108a7cf3d
rtos9er <rtos9er@chol.com>
2019-01-29 14:52:04 +0900

    mjs 커밋 오류.. 재 시도.

--- Tetris/Tetris.mjs
+++
@@ -1,7 +0,0 @@
-import TetrisModel from './TetrisModel.mjs'; 
-import TetrisView from './TetrisView.mjs';
-import TetrisViewModel from './TetrisViewModel.mjs';
-import TetrisPanel from './TetrisPanel.mjs';
-
-const tetrisViewModel = new TetrisViewModel('tetris');
-tetrisViewModel.run();
